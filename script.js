31a4162636962ef69847701cad8789b14bd466aa
competishun11 <166918321+competishun11@users.noreply.github.com>
2024-04-13 18:02:07 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let password = "";
+let password = '';
 
 document
   .getElementById('loginForm')
@@ -20,61 +20,67 @@ document
 
       var range = XLSX.utils.decode_range(sheet['!ref']);
 
+      var found = false;
+
       // Loop through each row to find the mobile number
       for (var rowNum = range.s.r + 1; rowNum <= range.e.r; rowNum++) {
         // Start from row 2 to skip header
         var mobileCell = sheet[XLSX.utils.encode_cell({ r: rowNum, c: 1 })]; // Assuming mobile number is in the second column
-        console.log(mobileCell.v, mobileNumber);
         if (mobileCell && mobileCell.v == mobileNumber) {
           var idCell = sheet[XLSX.utils.encode_cell({ r: rowNum, c: 2 })]; // Username
           var passwordCell = sheet[XLSX.utils.encode_cell({ r: rowNum, c: 3 })]; // Password
-        //   alert('Username: ' + idCell.v + '\nPassword: ' + passwordCell.v);
           var modalContent =
             'ID: ' + idCell.v + '<br>Password: ' + passwordCell.v;
-            password = passwordCell.v;
+          password = passwordCell.v;
           showModal(modalContent);
-          return;
+          found = true;
+          break;
         }
-    }
-    
-    // If mobile number not found
-  
-      showModal(
-        'Please Check your Registered Number. Please contact the support team at 7410833331, 7410833330'
-      );
-    
+      }
+
+      // If mobile number not found
+      if (!found) {
+        showModal(
+          'Please Check your Registered Number. Please contact the support team at 7410833331, 7410833330'
+        );
+      }
     };
 
     xhr.send();
   });
 
+function showModal(content) {
+  var modal = document.getElementById('myModal');
+  var modalContent = document.getElementById('modalContent');
+  modalContent.innerHTML = content;
+  modal.style.display = 'block';
 
-  function showModal(content) {
-    var modal = document.getElementById('myModal');
-    var modalContent = document.getElementById('modalContent');
-    modalContent.innerHTML = content;
-    modal.style.display = 'block';
+  var copyButton = document.getElementById('copyButton');
+  if (content.includes('Password')) {
+    copyButton.style.display = 'inline-block';
+  } else {
+    copyButton.style.display = 'none';
+  }
 
-    var closeBtn = document.getElementsByClassName('close')[0];
-    closeBtn.onclick = function () {
+  var closeBtn = document.getElementsByClassName('close')[0];
+  closeBtn.onclick = function () {
+    modal.style.display = 'none';
+  };
+
+  window.onclick = function (event) {
+    if (event.target == modal) {
       modal.style.display = 'none';
-    };
+    }
+  };
+}
 
-    window.onclick = function (event) {
-      if (event.target == modal) {
-        modal.style.display = 'none';
-      }
-    };
-  }
-
-  function copyToClipboard() {
-    var modalContent = document.getElementById('modalContent').innerText;
-    navigator.clipboard.writeText(modalContent).then(
-      function () {
-        // alert('Copied to clipboard!');
-      },
-      function () {
-        alert('Failed to copy to clipboard!');
-      }
-    );
-  }
+function copyToClipboard() {
+  navigator.clipboard.writeText(password).then(
+    function () {
+      alert('Password copied to clipboard!');
+    },
+    function () {
+      alert('Failed to copy password to clipboard!');
+    }
+  );
+}
